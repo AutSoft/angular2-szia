a048df86633fd6dcde31cb62500fbe21b45fdd18
Szupera Zita <szuperaz@gmail.com>
2017-01-17 15:11:16 +0100

    skeleton project

---
+++ app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: `
+        <div class="page-header">
+            <h1>SZIA
+                <small>South-Zubogy International Airport</small>
+            </h1>
+        </div>
+        <h1>SZIA</h1>
+  `,
+})
+export class AppComponent {}
---
+++ app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { Ng2BootstrapModule } from 'ng2-bootstrap';
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        Ng2BootstrapModule.forRoot(),
+    ],
+    declarations: [AppComponent],
+    bootstrap: [AppComponent]
+})
+export class AppModule {
+}
